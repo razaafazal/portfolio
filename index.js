c0c4dafe190fe5f5e2fa91f5e52765aafd4d2f04
Fazal Raza <162603506+razaafazal@users.noreply.github.com>
2024-03-24 20:23:15 +0530

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /* -----------------------------------------
   Have focus outline only for keyboard users 
  ---------------------------------------- */
-
+ 
 const handleFirstTab = (e) => {
   if(e.key === 'Tab') {
     document.body.classList.add('user-is-tabbing')
